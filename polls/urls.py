68345782f31686f73253f2bc94de495ffb5f56eb
shangkyu.shin <sskyu2@naver.com>
2022-09-11 13:20:51 +0900

    Second Comit

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,28 @@
 from django.urls import path
 
 from . import views
-
+app_name = 'polls'
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+
+# # 클래스 기반으로 작성하기 위해수정
+# from django.urls import path
+
+# from . import views
+
+# app_name = 'polls'
+# urlpatterns = [
+#     path('', views.IndexView.as_view(), name='index'),
+#     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+#     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,103 @@
-from django.shortcuts import render
+from cgi import print_arguments
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
-from django.http import HttpResponse
+from .models import Choice, Question
 
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
+
+# # 클래스 뷰(제너릭 뷰)
+# class IndexView(generic.ListView):
+#     template_name = 'polls/index.html'
+#     context_object_name = 'latest_question_list'
+
+#     def get_queryset(self):
+#         """Return the last five published questions."""
+#         return Question.objects.order_by('-pub_date')[:5]
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
+
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+# # 클래스 뷰(제너릭 뷰)
+# class DetailView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/detail.html'
+
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question.objects.filter(pub_date__lte=timezone.now()), pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+# # 클래스 뷰(제너릭 뷰)
+# class ResultsView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/results.html'
+
+# def vote(request, question_id):
+#     return HttpResponse("You're voting on question %s." % question_id)    
+
+def vote(request, question_id):
+    #get 방식으로 요청이 올 경우와  post방식으로 왔을때 처리 방법
+    if request.method=='GET':
+        print('get')
+    else:     # 'POST방식 일처리
+        print('post')
+
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
